1c095af8f2e27e96a97d4d2c6d01bcc5d3ae200f
Sophia Figueiredo Michel <sophia220059@ilum.cnpem.br>
2023-03-30 17:43:47 -0300

    experimento A.05

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -26,6 +26,18 @@ def gene_cnb(valor_max_caixa):
     gene = random.randint(0, valor_max_caixa)
     return gene
 
+def gene_letra(letras):
+    """Sorteia uma letra.
+    
+    Args:
+      letras: letras possíveis de serem sorteadas.
+      
+    Return:
+      Retorna uma letra dentro das possíveis de serem sorteadas.
+    """
+    letra = random.choice(letras)
+    return letra
+
 #####################
 #     INDIVÍDUOS    #
 #####################
@@ -62,6 +74,24 @@ def individuo_cnb(numero_genes, valor_max_caixa):
         individuo.append(gene)
         
     return individuo
+
+def individuo_senha(tamanho_senha, letras):
+    """Cria um candidato para o problema da senha
+    
+    Args:
+      tamanho_senha: inteiro representando o tamanho da senha.
+      letras: letras possíveis de serem sorteadas.
+      
+    Return:
+      Lista com n letras
+    """
+
+    candidato = []
+
+    for n in range(tamanho_senha):
+        candidato.append(gene_letra(letras))
+
+    return candidato
 
 ###################
 #    POPULAÇÃO    #
@@ -101,6 +131,22 @@ def populacao_cnb(tamanho_populacao, numero_genes, valor_max_caixa):
         
     return populacao
 
+def populacao_inicial_senha(tamanho, tamanho_senha, letras):
+    """Cria população inicial no problema de senha
+    
+    Args
+      tamanho: tamanho da população
+      tamanho_senha: inteiro representando o tamanho de senha
+      letras: letras possíveis de serem sorteadas
+      
+    Returns:
+      Lista com todos os indivíduos da população no problema de senha
+    """
+    populacao = []
+    for n in range(tamanho):
+        populacao.append(individuo_senha(tamanho_senha, letras))
+    return populacao
+
 #################
 #    SELEÇÃO    #
 #################
@@ -119,6 +165,46 @@ def selecao_roleta_max(populacao, fitness):
     """
     populacao_selecionada = random.choices(populacao, weights=fitness, k=len(populacao))
     return populacao_selecionada
+
+def selecao_torneio_min(populacao, fitness, tamanho_torneio=3):
+    """Faz a seleção de uma população usando torneio.
+    
+    Nota: da forma que está implementada, só funciona em problemas de
+    minimização.
+    
+    Args:
+      populacao: população do problema
+      fun_objetivo: função objetivo
+      tamanho_torneio: quantidade de invidiuos que batalham entre si
+      
+    Returns:
+      Individuos selecionados. Lista com os individuos selecionados com mesmo
+      tamanho do argumento `populacao`.
+    """
+    selecionados = []
+
+    # associando cada individuo com seu valor de fitness
+    par_populacao_fitness = list(zip(populacao, fitness))
+
+    # vamos fazer len(populacao) torneios
+    for _ in range(len(populacao)):
+        combatentes = random.sample(par_populacao_fitness, tamanho_torneio)
+
+        # é assim que se escreve infinito em python
+        minimo_fitness = float("inf")
+
+        for par_individuo_fitness in combatentes:
+            individuo = par_individuo_fitness[0]
+            fit = par_individuo_fitness[1]
+
+            # como queremos o individuo de menor fitness, então:
+            if fit < minimo_fitness:
+                selecionado = individuo
+                minimo_fitness = fit
+
+        selecionados.append(selecionado)
+
+    return selecionados
 
 ##################
 #   CRUZAMENTO   #
@@ -173,6 +259,20 @@ def mutacao_cnb(individuo, valor_max_caixa):
     
     return individuo
 
+def mutacao_senha(individuo, letras):
+    """Realiza a mutação de um gene no problema da senha.
+    
+    Args:
+      individuo: uma lista representado um individuo no problema da senha
+      letras: letras possíveis de serem sorteadas.
+      
+    Return:
+      Um individuo (senha) com um gene mutado.
+    """
+    gene = random.randint(0, len(individuo) - 1)
+    individuo[gene] = gene_letra(letras)
+    return individuo
+
 ###########################
 #    FUNÇÕES OBJETIVO     #
 ###########################
@@ -187,7 +287,41 @@ def funcao_objetivo_cb(individuo):
         Um valor rapresentando a soma dos genes do indivíduo.
     """
     return sum(individuo)
+
+def func_obj_cnb(individuo):
+    """Calcula o fitness do individuo para o problema das caixas não-binarias.
     
+    Args:
+        individuo: lista que representa um individuo dentro do problema
+        
+    Retunrs:
+        Um valor que representando o fitness de cada individuo
+    """
+    fitness = sum (individuo)
+    return fitness
+
+def func_obj_senha(individuo, senha_verdadeira):
+    """Computa a função objetivo de uma indivíduo no problema da senha
+    
+    Args:
+        individuo: lista contendo as letras da senha
+        senha_verdadeira: a senha que você está tentando descobrir
+        
+    Returns:
+        A "distância" entre a senha proposta e a senha verdadeira. Essa distância é medida letra por letra. 
+        Quanto mais distante uma letra for da que deveria ser, maior é essa distância.
+    """
+    diferenca = 0
+    
+    for letra_candidato, letra_oficial in zip(individuo, senha_verdadeira):
+        diferenca = diferenca + abs(ord(letra_candidato) - ord(letra_oficial))
+        
+    return diferenca
+
+#######################################
+#    FUNÇÕES OBJETIVO - população     #
+#######################################
+
 def funcao_objetivo_pop_cb(populacao):
     """Calcula a funcao objetivo para todos os membres de uma população.
     
@@ -201,18 +335,6 @@ def funcao_objetivo_pop_cb(populacao):
     for individuo in populacao:
         fun_ob = funcao_objetivo_cb(individuo)
         fitness.append(fun_ob)
-    return fitness
-
-def func_obj_cnb(individuo):
-    """Calcula o fitness do individuo para o problema das caixas não-binarias.
-    
-    Args:
-        individuo: lista que representa um individuo dentro do problema
-        
-    Retunrs:
-        Um valor que representando o fitness de cada individuo
-    """
-    fitness = sum (individuo)
     return fitness
 
 def func_obj_pop_cnb(populacao):
@@ -230,3 +352,20 @@ def func_obj_pop_cnb(populacao):
         fitness_pop.append(fitness_ind)
     
     return fitness_pop
+
+def func_obj_pop_senha(populacao, senha_verdadeira):
+    """Computa a funcao objetivo de uma populaçao no problema da senha
+    
+    Args:
+      populacao: lista com todos os individuos da população
+      senha_verdadeira: a senha que você está tentando descobrir
+      
+    Returns:
+      Lista contendo os valores da métrica de distância entre senhas
+    """
+    resultado = []
+
+    for individuo in populacao:
+        resultado.append(func_obj_senha(individuo, senha_verdadeira))
+
+    return resultado
