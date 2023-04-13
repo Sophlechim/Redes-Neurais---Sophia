3bc625da1e2bfd5e7d2cf96bb1f04a27e37e31bc
Sophia Figueiredo Michel <sophia220059@ilum.cnpem.br>
2023-04-13 17:28:12 -0300

    experimento 7

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -43,6 +43,36 @@ def cria_cidades(n):
 
     return cidades
 
+def computa_mochila(individuo, objetos, ordem_dos_nomes):
+    """Computa o valor total e peso total de uma mochila
+    
+    Args:
+      individiuo:
+        Lista binária contendo a informação de quais objetos serão selecionados.
+      objetos:
+        Dicionário onde as chaves são os nomes dos objetos e os valores são
+        dicionários com a informação do peso e valor.
+      ordem_dos_nomes:
+        Lista contendo a ordem dos nomes dos objetos.
+        
+    Returns:
+      valor_total: valor total dos itens da mochila em unidades de dinheiros.
+      peso_total: peso total dos itens da mochila em unidades de massa.
+    """
+
+    valor_total = 0
+    peso_total = 0
+    
+    for pegou_o_item_ou_nao, nome_do_item in zip(individuo, ordem_dos_nomes):
+        if pegou_o_item_ou_nao == 1:
+            valor_do_item = objetos[nome_do_item]["valor"]
+            peso_do_item = objetos[nome_do_item]["peso"]
+            
+            valor_total = valor_total + valor_do_item
+            peso_total = peso_total + peso_do_item
+    
+    return valor_total, peso_total
+
 
 ###################
 #      GENES      #
@@ -285,6 +315,46 @@ def selecao_torneio_min(populacao, fitness, tamanho_torneio=3):
 
     return selecionados
 
+# FUNÇAO PARA DESAFIO CAIXEIRO INCONsCIENTE
+def selecao_torneio_max(populacao, fitness, tamanho_torneio=3):
+    """Faz a seleção de uma população usando torneio.
+    
+    Nota: da forma que está implementada, essa função servirá para maximização.
+    
+    Args:
+      populacao: população do problema
+      fun_objetivo: função objetivo
+      tamanho_torneio: quantidade de invidiuos que batalham entre si
+      
+    Returns:
+      Individuos selecionados. Lista com os individuos selecionados com mesmo
+      tamanho do argumento `populacao`.
+    """
+    selecionados = []
+
+    # associando cada individuo com seu valor de fitness
+    par_populacao_fitness = list(zip(populacao, fitness))
+
+    # vamos fazer len(populacao) torneios
+    for _ in range(len(populacao)):
+        combatentes = random.sample(par_populacao_fitness, tamanho_torneio)
+
+        # é assim que se escreve infinito em python
+        maximo_fitness = -float("inf")
+
+        for par_individuo_fitness in combatentes:
+            individuo = par_individuo_fitness[0]
+            fit = par_individuo_fitness[1]
+
+            # como queremos o individuo de menor fitness, então:
+            if fit > maximo_fitness:
+                selecionado = individuo
+                maximo_fitness = fit
+
+        selecionados.append(selecionado)
+
+    return selecionados
+
 ##################
 #   CRUZAMENTO   #
 ##################
@@ -404,6 +474,7 @@ def mutacao_de_troca(individuo):
     individuo[indice1], individuo[indice2] = individuo[indice2], individuo[indice1]
     return individuo
 
+
 ###########################
 #    FUNÇÕES OBJETIVO     #
 ###########################
@@ -453,10 +524,8 @@ def funcao_objetivo_cv(individuo, cidades):
     """Computa a funcao objetivo de um individuo no problema do caixeiro viajante.
     
     Args:
-      individiuo:
-        Lista contendo a ordem das cidades que serão visitadas
-      cidades:
-        Dicionário onde as chaves são os nomes das cidades e os valores são as
+      individiuo: lista contendo a ordem das cidades que serão visitadas
+      cidades: dicionário onde as chaves são os nomes das cidades e os valores são as
         coordenadas das cidades.
     
     Returns:
@@ -483,6 +552,29 @@ def funcao_objetivo_cv(individuo, cidades):
     distancia = distancia + percurso
 
     return distancia
+
+def funcao_objetivo_mochila(individuo, objetos, limite, ordem_dos_nomes):
+    """Computa a funcao objetivo de um candidato no problema da mochila.
+    
+    Args:
+      individiuo: lista binária contendo a informação de quais objetos serão selecionados.
+      objetos: dicionário onde as chaves são os nomes dos objetos e os valores são
+        dicionários com a informação do peso e valor.
+      limite: número indicando o limite de peso que a mochila aguenta.
+      ordem_dos_nomes: lista contendo a ordem dos nomes dos objetos.
+        
+    Returns:
+      Valor total dos itens inseridos na mochila considerando a penalidade para
+      quando o peso excede o limite.
+    """
+
+    valor_mochila, peso_mochila = computa_mochila(individuo, objetos, ordem_dos_nomes)
+    
+    if peso_mochila > limite:
+        return 0.01
+    else:
+        return valor_mochila
+    
 
 #######################################
 #    FUNÇÕES OBJETIVO - população     #
@@ -554,5 +646,33 @@ def funcao_objetivo_pop_cv(populacao, cidades):
     resultado = []
     for individuo in populacao:
         resultado.append(funcao_objetivo_cv(individuo, cidades))
+
+    return resultado
+
+def funcao_objetivo_pop_mochila(populacao, objetos, limite, ordem_dos_nomes):
+    """Computa a fun. objetivo de uma populacao no problema da mochila
+    
+    Args:
+      populacao:
+        Lista com todos os individuos da população
+      objetos:
+        Dicionário onde as chaves são os nomes dos objetos e os valores são
+        dicionários com a informação do peso e valor.
+      limite:
+        Número indicando o limite de peso que a mochila aguenta.
+      ordem_dos_nomes:
+        Lista contendo a ordem dos nomes dos objetos.
+    
+    Returns:
+      Lista contendo o valor dos itens da mochila de cada indivíduo.
+    """
+
+    resultado = []
+    for individuo in populacao:     # navegue a funçao objetivo dos indivíduos
+        resultado.append(           # adicione 
+            funcao_objetivo_mochila(
+                individuo, objetos, limite, ordem_dos_nomes
+            )
+        )
 
     return resultado
